7b0bd4b2f4b39bb796e2b33341e4a02207346fa6
luizmiguelfb <miguelfranciscobelmock@gmail.com>
2025-08-29 11:53:06 -0300

    Primeiro commit

---
+++ pagina/index.js
@@ -0,0 +1,32 @@
+let tradutor = document.getElementById('tradutor');
+tradutor.addEventListener('click', function() {
+    window.open("/img/5313f8d34ee82.jpeg", "_self");
+});
+
+let gmail = document.querySelector('#form');
+gmail.addEventListener('submit', (e) => { 
+    e.preventDefault();
+
+    emailjs.init({
+        publicKey: 'lhqvIe35GivxFJF0D'
+    });
+
+    emailjs.sendForm("service_w1iv7hg", "template_bfjah0k", form)
+        .then((response) => alert('Mensagem enviada com sucesso!'))
+        .catch((error) => {
+            console.log(error);
+            alert('Erro ao enviar a mensagem!');
+        });
+
+});
+
+const nome = document.getElementById('nome');
+const email = document.getElementById('email');
+const mensagem = document.getElementById('mensagem');
+const limpar = document.getElementById('enviar');
+
+limpar.addEventListener('click', () => {
+    nome.value = ''; 
+    email.value = '';
+    mensagem.value = '';
+})
---
+++ pagina/temp.js
@@ -0,0 +1,3 @@
+// setTimeout(function(){
+//   location.reload(); // Recarrega a página
+// }, 5000); // Recarrega a página após 5 segundos
